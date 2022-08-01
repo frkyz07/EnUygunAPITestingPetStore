e9133c3e0be2fb0bb0442f5a11d0504fd15ed652
farukayaz <frkyz07.13@gmail.com>
2022-08-01 09:27:20 +0300

    - some fixes for the HttpEntityTests i will continuie to fix

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,6 +1,18 @@
 package models;
 
+import lombok.Data;
+
 public class User {
 
-    public String username, email,password;
+    @Data
+    public class request{
+        private int id;
+        private int invalidId;
+        private int petID;
+        private int quantity;
+        private String shipDate;
+        private String status;
+        private boolean complete;
+
+    }
 }
--- src/test/java/HttpEntityTests.java
+++ src/test/java/HttpEntityTests.java
@@ -1,41 +1,48 @@
 import com.github.javafaker.Faker;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpHeaders;
+import org.json.JSONObject;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.testng.annotations.Test;
-
-import java.awt.*;
 
 // i wasnt able to finish this part of the project so sorry for that
 public class HttpEntityTests {
 
     HttpHeaders headers;
     RestTemplate restTemplate;
+    ObjectMapper objectMapper;
+    JSONObject jsonObject;
     Faker faker = new Faker();
-    String id = faker.address().buildingNumber();
-    String invalidId = ("-"+id);
-    String petId = faker.address().buildingNumber();
-    String body = "{\n  \"id\":"+id+",\n  \"petId\": "+petId+",\n  \"quantity\": 1,\n  \"shipDate\": \"2022-07-29T15:15:50.460+0000\",\n  \"status\": \"placed\",\n  \"complete\": true\n}";
+    private Faker fakeValuesService;
+    int id = Integer.parseInt(faker.address().buildingNumber());
+    int petId = this.faker.number().randomDigitNotZero();
+    String shipDate = this.fakeValuesService.bothify("####-##-29T13:##:##.####");
+
+    //String body = "{\n  \"id\":"+id+",\n  \"petId\": "+petId+",\n  \"quantity\": 1,\n  \"shipDate\": \"2022-07-29T15:15:50.460+0000\",\n  \"status\": \"placed\",\n  \"complete\": true\n}";
     String post_endpoint = "/store/order/";
     String inventory_endpoint = "/store/inventory";
     String url = "https://petstore.swagger.io/v2";
+    String invalidId = ("-"+id);
 
 
-    String invalid_body = "{\n  \"id\":"+invalidId+",\n  \"petId\": "+petId+",\n  \"quantity\": 1,\n  \"shipDate\": \"2022-07-29T15:15:50.460+0000\",\n  \"status\": \"placed\",\n  \"complete\": true\n}";
-    public void ApiTest(){
 
-        headers.setContentType(PageAttributes.MediaType);
+    //String invalid_body = "{\n  \"id\":"+invalidId+",\n  \"petId\": "+petId+",\n  \"quantity\": 1,\n  \"shipDate\": \"2022-07-29T15:15:50.460+0000\",\n  \"status\": \"placed\",\n  \"complete\": true\n}";
+    public HttpEntityTests(Faker fakeValuesService){
+        this.fakeValuesService = fakeValuesService;
+        this.headers = new HttpHeaders();
+        this.objectMapper = new ObjectMapper();
+        this.jsonObject = new JSONObject();
+        this.restTemplate = new RestTemplate();
+
     }
 
     @Test
     public String valid_post_request(){
 
 
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.set("headers1", "headers1");
 
             HttpEntity entity = new HttpEntity(body, headers);
             ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.POST, (org.springframework.http.HttpEntity<?>) entity, String.class);
